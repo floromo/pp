2bd659bc9f2d53048138c0cea9d2bc0219429d20
floromo <flormmmp@gmail.com>
2023-10-16 17:09:21 -0700

    balance

--- src/main/java/frc/robot/commands/StraightLine.java
+++ src/main/java/frc/robot/commands/StraightLine.java
@@ -9,18 +9,17 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.math.controller.PIDController;
 
 /** An example command that uses an example subsystem. */
-public class Balance extends CommandBase {
+public class StraightLine extends CommandBase {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
   private final RomiDrivetrain m_subsystem;
-  private boolean isDetected;
-  private PIDController m_balance = new PIDController(0, 0, 0);
+  private PIDController m_straight = new PIDController(0, 0, 0);
 
   /**
    * Creates a new ExampleCommand.
    *
    * @param subsystem The subsystem used by this command.
    */
-  public Balance (RomiDrivetrain subsystem) {
+  public StraightLine (RomiDrivetrain subsystem) {
     m_subsystem = subsystem;
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(subsystem);
@@ -35,23 +34,13 @@ public class Balance extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-
-    if (m_subsystem.m_Gyro.getAngleY() <= 11) {
-      m_subsystem.arcadeDrive(0.5, 0);
-    } else {
-      isDetected = true;
-    }
-
-    if (isDetected = true) {
-    m_subsystem.arcadeDrive(m_balance.calculate(m_subsystem.m_Gyro.getAngleY(),0), m_balance.calculate(m_subsystem.m_Gyro.getAngleZ(),0));
-    }
-
+    m_subsystem.arcadeDrive(0.5, m_straight.calculate(m_subsystem.m_Gyro.getAngleZ(),0));
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    m_subsystem.arcadeDrive(0, 0);
+    m_subsystem.arcadeDrive(0,0);
   }
 
   // Returns true when the command should end.
